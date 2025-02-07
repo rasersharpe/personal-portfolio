e9b538158977415cd0c300010f0fbb0ee57dcb98
rasersharpe <jay.bhatt@me.com>
2025-02-06 20:26:19 -0600

    updated all pages

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,44 @@
 import React from "react";
 
+import "./About.css";
+
 const About = () => {
-  return <div>About</div>;
+  return (
+    <main>
+      <section className="aboutMe">
+        <div>
+          <p>
+            Hi, I’m Jay, a passionate coding bootcamp student diving into the
+            world of software development. With no prior experience, I’m excited
+            to embrace new challenges and continuously learn as I build my
+            skills.
+            <br />
+            Before starting my coding journey, I was a lead editor for animated
+            film and TV. These experiences taught me how to handle deadlines,
+            how to deal with a stressful environment and how to lead a team
+            which I now apply to my coding projects.
+            <br />
+            Currently, I’m mastering technologies like React, Node.js, and
+            PostgreSQL while working on projects like a CMS Employee Tracker and
+            my personal portfolio website.
+            <br />
+            My goal is to become a full-stack developer and contribute to
+            building applications that solve real-world problems. I’m especially
+            fascinated by creating intuitive user interfaces, and scalable
+            back-end systems.
+            <br />
+            When I’m not coding, you can find me exploring fantasy worlds like
+            Game of Thrones or rewatching my favorite movie, The Matrix. I also
+            enjoy hiking, photography, and gaming.
+            <br />
+            I’m always open to learning from others and collaborating on
+            exciting projects. Feel free to reach out through the links at the
+            bottom or the contact page!
+          </p>
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default About;
